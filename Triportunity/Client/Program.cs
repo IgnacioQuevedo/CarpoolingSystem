de51277e13e2fd5d9b08e133ef9cb8aae4c72a74
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-03-20 22:29:03 -0300

    Extracted functionalities to methods

--- Triportunity/Client/Program.cs
+++ Triportunity/Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Client.Services;
 
 namespace Client
@@ -8,10 +9,10 @@ namespace Client
         public static void Main(string[] args)
         {
             string optionSelected = "";
-            bool executeOptions = true;
+            bool appFunctional = true;
             
             
-            while (executeOptions)
+            while (appFunctional)
             {
                 MainMenuMsgs();
                 optionSelected = Console.ReadLine();
@@ -23,28 +24,49 @@ namespace Client
                         break;
 
                     case "2":
-                        Console.WriteLine("Your username will be:");
-                        string usernameRegister = Console.ReadLine();
-                        Console.WriteLine("Register your password:");
-                        string passwordRegister = Console.ReadLine();
-                        Console.WriteLine("Insert the same password as above:");
-                        string repeatedPassword = Console.ReadLine();
-                        //ServiceMethod that will create the user.
+                        RegisterMsgs();
                         break;
                     
                     case "3":
-                        Console.WriteLine("lorem ipsum dolor");
-                        Console.WriteLine("Press 1 to go back to main menu");
+                        AboutUsMsgs();
                         break;
                     case "4":
-                        Console.WriteLine("Closing");
-                        executeOptions = false;
+                        appFunctional = CloseApp();
                         break;
                     default:
                         Console.WriteLine("Insert a valid digit, please.");
                         break;
                 }
             }
+        }
+
+        private static bool CloseApp()
+        {
+            bool closeApp;
+            Console.WriteLine("Closing");
+            for (int i = 0; i < 8; i++)
+            {
+                Thread.Sleep(300);
+                Console.WriteLine(".");
+            }
+            return false;
+        }
+
+        private static void AboutUsMsgs()
+        {
+            Console.WriteLine("lorem ipsum dolor");
+            Console.WriteLine("Press 1 to go back to main menu");
+        }
+
+        private static void RegisterMsgs()
+        {
+            Console.WriteLine("Your username will be:");
+            string usernameRegister = Console.ReadLine();
+            Console.WriteLine("Register your password:");
+            string passwordRegister = Console.ReadLine();
+            Console.WriteLine("Insert the same password as above:");
+            string repeatedPassword = Console.ReadLine();
+            //ServiceMethod that will create the user.
         }
 
         private static void LoginMsgs()
