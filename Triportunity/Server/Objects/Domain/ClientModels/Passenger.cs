f2888b3203fbf2307f19ba87d9f2ce60d779bad4
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-03-22 01:06:17 -0300

    Removed wrong property on passenger

--- Triportunity/Server/Objects/Domain/ClientModels/Passenger.cs
+++ Triportunity/Server/Objects/Domain/ClientModels/Passenger.cs
@@ -4,12 +4,10 @@ namespace Server.Objects.Domain.ClientModels
 {
     public class Passenger : Client
     {
-        public Ride RideJoined { get; set; }
         public int SeatNumber { get; set; }
         
-        public Passenger(string username,string password,Ride rideJoined, int seatNumber) : base(username,password)
+        public Passenger(string username,string password, int seatNumber) : base(username,password)
         {
-            RideJoined = rideJoined;
             SeatNumber = seatNumber;
         }
     }
