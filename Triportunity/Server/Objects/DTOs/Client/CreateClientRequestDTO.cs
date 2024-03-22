269d8c774b56840dfd6eba74944e76e7ad764cbe
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-03-22 00:31:35 -0300

    Created CreateClientRequestDto class

--- Triportunity/Server/Objects/DTOs/Client/CreateClientRequestDTO.cs
+++ Triportunity/Server/Objects/DTOs/Client/CreateClientRequestDTO.cs
@@ -1,7 +1,14 @@
 namespace Server.Objects.DTOs.Client
 {
-    public class CreateClientRequestDTO
+    public class CreateClientRequestDto
     {
-        
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public CreateClientRequestDto(string username,string password)
+        {
+            Username = username;
+            Password = password;
+        }
     }
 }
