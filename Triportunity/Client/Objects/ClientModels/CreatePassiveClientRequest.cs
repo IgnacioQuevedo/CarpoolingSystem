3e114f51298d6185f1be0793a77f51ddaf6e5210
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-03-22 01:20:02 -0300

    Created loginRequest in frontend

--- Triportunity/Client/Objects/ClientModels/CreatePassiveClientRequest.cs
+++ Triportunity/Client/Objects/ClientModels/CreatePassiveClientRequest.cs
@@ -1,4 +1,4 @@
-namespace Client.Objects
+namespace Client.Objects.ClientModels
 {
     public class CreatePassiveClientRequest
     {
--- Triportunity/Client/Objects/ClientModels/LoginPassiveClientRequest.cs
+++ Triportunity/Client/Objects/ClientModels/LoginPassiveClientRequest.cs
@@ -1,15 +1,12 @@
 namespace Client.Objects.ClientModels
 {
-    public class Client
+    public class LoginPassiveClientRequest
     {
-        public string Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
 
-
-        public Client(string id,string username,string password)
+        public LoginPassiveClientRequest(string username,string password)
         {
-            Id = id;
             Username = username;
             Password = password;
         }
