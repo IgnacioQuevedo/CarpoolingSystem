fb8e89f492039888e442e0524ae01fd4a6c1c8c2
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-03-21 23:44:04 -0300

    Implemented user properties and ctor

---
+++ Triportunity/Server/Objects/Domain/Client.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Server.Objects.Domain
+{
+    public class Client
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        
+        public Client(string username,string password)
+        {
+            Id = new Guid();
+            Username = username;
+            Password = password;
+        }
+    }
+}
