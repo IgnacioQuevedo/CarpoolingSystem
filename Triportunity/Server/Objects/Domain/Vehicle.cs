4c97669936d46c901b0b7c23dc6d9539f5788889
FGhione <fghione18@gmail.com>
2024-03-24 22:55:35 -0300

    Setting properties to new class Vehicle

---
+++ Triportunity/Server/Objects/Domain/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Server.Objects.Domain
+{
+    public class Vehicle
+    {
+
+        public Guid Id { get; set; }
+
+        public VehicleImage Picture { get; set; }
+
+        public Vehicle(VehicleImage picture)
+        {
+            Picture = picture;
+        }
+    }
+}
