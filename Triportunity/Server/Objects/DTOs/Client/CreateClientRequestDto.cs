7956c9c08932f870b2ccfb30c5e56626d716a949
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-03-25 18:50:16 -0300

    Updated createClientRequestDto

--- Triportunity/Server/Objects/DTOs/Client/CreateClientRequestDto.cs
+++ Triportunity/Server/Objects/DTOs/Client/CreateClientRequestDto.cs
@@ -1,14 +1,20 @@
+#nullable enable
+using System.Collections.Generic;
+using Server.Objects.Domain;
+
 namespace Server.Objects.DTOs.Client
 {
     public class CreateClientRequestDto
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public ICollection<VehicleDto>? Vehicles { get; set; }
 
-        public CreateClientRequestDto(string username,string password)
+        public CreateClientRequestDto(string username,string password, ICollection<VehicleDto> clientVehicles)
         {
             Username = username;
             Password = password;
+            Vehicles = clientVehicles;
         }
     }
 }
