c9005bba9b96619254872ab6533529351c05ca65
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-03-25 17:39:06 -0300

    Implemented ctor of DriverInfo

--- Triportunity/Server/Objects/Domain/DriverInfo.cs
+++ Triportunity/Server/Objects/Domain/DriverInfo.cs
@@ -1,16 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Server.Objects.Domain
 {
     public class DriverInfo
     {
+        public Guid Id { get; set; }
         public int Ci { get; set; }
-        public int Puntuation { get; set; }
-        public IEnumerable<Review> Reviews { get; set; }
-        public IEnumerable<Vehicle> Vehicles { get; set; }
-        
-        
-        
+        public double Puntuation { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+        public ICollection<Vehicle> Vehicles { get; set; }
+
+
+        public DriverInfo(int ci,ICollection<Vehicle> driverVehicles)
+        {
+            Ci = ci;
+            Puntuation = 5.0;
+            Reviews = new List<Review>();
+            Vehicles = new List<Vehicle>();
+
+            for (int i = 0; i < driverVehicles.Count(); i++)
+            {
+                Vehicles.Add(driverVehicles.ElementAtOrDefault(i));
+            }
+
+        }
     }
 }
