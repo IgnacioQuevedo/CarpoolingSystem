23e940069a1fcdcc3e15e3c324e460eacb183f55
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2024-03-25 00:53:22 -0300

    Added properties for ride on domain

--- Triportunity/Server/Objects/Domain/Ride.cs
+++ Triportunity/Server/Objects/Domain/Ride.cs
@@ -1,7 +1,21 @@
+using System;
+using System.Runtime.Remoting.Channels;
+
 namespace Server.Objects.Domain
 {
     public class Ride
     {
-        
+        //public Client Dirver { get; set; }
+        //public List<Client> Passengers { get; set; }
+        public string InitialLocation { get; set; }
+        public string EndingLocation { get; set; }
+        public DateTime RideDate { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public int AvailableSeats { get; set; }
+        public int TotalSeats { get; set; }
+        public double PricePerPerson { get; set; }
+        public bool PetsAllowed { get; set; }
+        public string PhotoPath { get; set; }
+
     }
 }
