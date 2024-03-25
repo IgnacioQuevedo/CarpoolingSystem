47971099addc7f9b83d712d53263cf031f083943
FGhione <fghione18@gmail.com>
2024-03-24 22:54:17 -0300

    Setting properties to new class VehicleImage

---
+++ Triportunity/Server/Objects/Domain/VehicleImage.cs
@@ -0,0 +1,19 @@
+﻿namespace Server.Objects.Domain
+{
+    public class VehicleImage
+    {
+
+        public string FileName { get; set; }
+
+        public string FileExtension { get; set; }
+
+        public string Url { get; set; }
+
+        public VehicleImage(string fileName, string fileExtension, string url)
+        {
+            FileName = fileName;
+            FileExtension = fileExtension;
+            Url = url;
+        }
+    }
+}
