8b21a3aecac29fcae12d83a7e2410b8feb59edc0
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-03-26 02:26:20 -0300

    Adapted method so now is more generic

--- Triportunity/Server/Objects/Domain/ClientModels/Client.cs
+++ Triportunity/Server/Objects/Domain/ClientModels/Client.cs
@@ -36,23 +36,24 @@ namespace Serverg.Objects.Domain.ClientModels
         private void PasswordValidation()
         {
             const int validLengthForPassword = 6;
-
+            const int amountOfNumbersOnPassword = 4;
             if (Password.Length < 6)
             {
                 throw new ClientException("Password must be greater than: " + Password.Length + "digits");
             }
             
             // Expresión regular que busca al menos 4 números y un símbolo especial
-            ValidateStringContains4DigitsAndSpecialCharacter();
+            ValidatePasswordContainsXNumbersAndSpecialCharacter(amountOfNumbersOnPassword);
         }
 
-        private void ValidateStringContains4DigitsAndSpecialCharacter()
+        private void ValidatePasswordContainsXNumbersAndSpecialCharacter(int amountOfNumbers)
         {
-            string patron = @"^(?=.*\d{4,})(?=.*[^\w\d\s])";
-
-            if (!Regex.IsMatch(Password, patron))
+            string pattern = $@"^(?=(?:\D*\d){{{amountOfNumbers},}})(?=.*[^\w\d\s])";
+            
+            if (!Regex.IsMatch(Password, pattern))
             {
-                throw new ClientException("Password must contain 4 digits and an special character!");
+                throw new ClientException("Password must contain: " + amountOfNumbers + 
+                                          "digits and at least an special character!");
             }
         }
     }
