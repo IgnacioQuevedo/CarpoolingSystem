48713092875bae0bee6a779d6546dc7ce8f7c03e
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-03-26 00:07:37 -0300

    Implemented instance of memory database

--- Triportunity/Server/DataContext/MemoryDatabase.cs
+++ Triportunity/Server/DataContext/MemoryDatabase.cs
@@ -1,7 +1,31 @@
+using System.Collections.Generic;
+using Server.Objects.Domain;
+using Serverg.Objects.Domain.ClientModels;
+
 namespace Server.DataContext
 {
     public class MemoryDatabase
     {
+        public ICollection<Client> Clients { get; set; }
+        public ICollection<Ride> Rides { get; set; }
+
+        private static MemoryDatabase _database;
+
+        private MemoryDatabase()
+        {
+            Clients = new List<Client>();
+            Rides = new List<Ride>();
+        }
+        public static MemoryDatabase GetInstance()
+        {
+            if (_database is null)
+            {
+                _database = new MemoryDatabase();
+            }
+
+            return _database;
+        }
+        
         
     }
 }
