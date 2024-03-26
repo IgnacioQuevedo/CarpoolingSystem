bc8db52dde13a7db88b3671c3beaeefc45dd8560
FGhione <fghione18@gmail.com>
2024-03-26 15:41:24 -0300

    created Vehicle domain models in client project

---
+++ Triportunity/Client/Objects/VehicleModels/VehicleImage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Objects.VehicleModels
+{
+    public class VehicleImage
+    {
+        public string FileName { get; set; }
+
+        public string FileExtension { get; set; }
+
+        public string Url { get; set; }
+
+        public VehicleImage(string fileName, string fileExtension, string url)
+        {
+            FileName = fileName;
+            FileExtension = fileExtension;
+            Url = url;
+        }
+    }
+}
