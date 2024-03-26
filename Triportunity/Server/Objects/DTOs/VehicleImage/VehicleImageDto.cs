300b7442deaca5c081a900224a91f96ee8f870c4
FGhione <fghione18@gmail.com>
2024-03-26 14:57:35 -0300

    Creating DTO for VehicleImage class

---
+++ Triportunity/Server/Objects/DTOs/VehicleImage/VehicleImageDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Objects.DTOs.VehicleImage
+{
+    public class VehicleImageDto
+    {
+
+        public string FileName { get; set; }
+
+        public string FileExtension { get; set; }
+
+        public string Url { get; set; }
+
+        public VehicleImageDto(string fileName, string fileExtension, string url)
+        {
+            FileName = fileName;
+            FileExtension = fileExtension;
+            Url = url;
+        }
+
+    }
+}
