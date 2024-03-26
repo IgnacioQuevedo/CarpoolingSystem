4a67d1e20d6b25e9324636a313b89fe37dd0d21a
FGhione <fghione18@gmail.com>
2024-03-26 15:25:20 -0300

    created new DTOs for create request of Vehicle and VehicleImage

---
+++ Triportunity/Server/Objects/DTOs/Vehicle/CreateVehicleRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using Server.Objects.DTOs.VehicleImage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Objects.DTOs.Vehicle
+{
+    public class CreateVehicleRequestDto
+    {
+
+        public CreateVehicleImageRequestDto  Picture { get; set; }
+
+        public CreateVehicleRequestDto(CreateVehicleImageRequestDto picture) {
+            Picture = picture;
+        }
+    }
+}
