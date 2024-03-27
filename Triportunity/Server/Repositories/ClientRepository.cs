2f347ff4cfb4cb64da5ac4fc53396099204aeb97
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-03-27 15:55:46 -0300

    Register method is now functional

--- Triportunity/Server/Repositories/ClientRepository.cs
+++ Triportunity/Server/Repositories/ClientRepository.cs
@@ -1,11 +1,19 @@
+using Server.DataContext;
 using Server.Objects.Domain.ClientModels;
 
 namespace Server.Repositories
 {
     public class ClientRepository
     {
-       
-
+        private readonly object _registerLock = new object(); 
+        
+        public void RegisterClient(Client clientToRegister)
+        {
+            lock (_registerLock)
+            {
+                MemoryDatabase.GetInstance().Clients.Add(clientToRegister);
+            }
+        }
         
         
         
