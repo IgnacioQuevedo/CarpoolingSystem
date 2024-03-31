1d6fe62716c9b34a3ad481dfad73ce5d21b52ade
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-03-31 19:00:35 -0300

    Created driverInfo class

---
+++ Triportunity/Client/Objects/ClientModels/DriverInfo.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Client.Objects.VehicleModels;
+
+namespace Client.Objects.ClientModels
+{
+    public class DriverInfo
+    {
+        public string Ci { get; set; }
+        public double Puntuation { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+        public ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
