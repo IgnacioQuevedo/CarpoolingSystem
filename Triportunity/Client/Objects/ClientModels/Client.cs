3edee521433132f2574b1fcbc96acba6e54b2092
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-03-31 18:58:34 -0300

    Create client class

--- Triportunity/Client/Objects/ClientModels/Client.cs
+++ Triportunity/Client/Objects/ClientModels/Client.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace Client.Objects.ClientModels
 {
     public class Client
     {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }    
+        public DriverInfo? DriverAspects { get; set; }
         
     }
 }
