a79caf961078bf46e17751bcb2d6fd5832bbfce2
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-03-31 18:53:32 -0300

    Refactored name of createClienRequestDto to RegisterClientRequestDto

---
+++ Triportunity/Client/Objects/ClientModels/Client.cs
@@ -0,0 +1,7 @@
+namespace Client.Objects.ClientModels
+{
+    public class Client
+    {
+        
+    }
+}
--- Triportunity/Server/Objects/DTOs/ClientModelDtos/RegisterClientRequestDto.cs
+++ Triportunity/Server/Objects/DTOs/ClientModelDtos/RegisterClientRequestDto.cs
@@ -1,13 +1,13 @@
 #nullable enable
 namespace Server.Objects.DTOs.ClientModelDtos
 {
-    public class CreateClientRequestDto
+    public class RegisterClientRequestDto
     {
         public string Username { get; set; }
         public string Password { get; set; }
         public CreateDriverInfoRequestDto? DriverAspects { get; set; }
 
-        public CreateClientRequestDto(string username, string password, CreateDriverInfoRequestDto? driverAspects)
+        public RegisterClientRequestDto(string username, string password, CreateDriverInfoRequestDto? driverAspects)
         {
             Username = username;
             Password = password;
