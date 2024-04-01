201c2e7a3c354ea4f84c92971c2e7ac15aef9af3
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2024-04-01 19:22:02 -0300

    Corrected ride domain logic using enum

--- Triportunity/Server/Objects/Domain/Ride.cs
+++ Triportunity/Server/Objects/Domain/Ride.cs
@@ -1,5 +1,6 @@
 using Server.Exceptions;
 using Server.Objects.Domain.ClientModels;
+using Server.Objects.Domain.Enums;
 using System;
 using System.Collections.Generic;
 
@@ -10,8 +11,8 @@ namespace Server.Objects.Domain
         public Guid Id { get; set; }
         public Client Driver { get; set; }
         public List<Client> Passengers { get; set; }
-        public string InitialLocation { get; set; }
-        public string EndingLocation { get; set; }
+        public CitiesEnum InitialLocation { get; set; }
+        public CitiesEnum EndingLocation { get; set; }
         public DateTime DepartureTime { get; set; }
         public int AvailableSeats { get; set; }
         public int TotalSeats { get; set; }
@@ -19,7 +20,7 @@ namespace Server.Objects.Domain
         public bool PetsAllowed { get; set; }
         public string PhotoPath { get; set; }
 
-        public Ride(Client driver, List<Client> passengers, string initialLocation, string endingLocation, DateTime departureTime, int availableSeats, int totalSeats, double pricePerPerson, bool petsAllowed, string photoPath)
+        public Ride(Client driver, List<Client> passengers, CitiesEnum initialLocation, CitiesEnum endingLocation, DateTime departureTime, int availableSeats, int totalSeats, double pricePerPerson, bool petsAllowed, string photoPath)
         {
             Id = new Guid();
             Driver = driver;
@@ -45,11 +46,11 @@ namespace Server.Objects.Domain
 
         private void LocationValidator()
         {
-            int minLengthOfLocation = 1;
+            bool bothLocationsBelongToCitiesEnum = Enum.IsDefined(typeof(CitiesEnum), EndingLocation) && Enum.IsDefined(typeof(CitiesEnum), InitialLocation);
 
-            if (EndingLocation.Length < minLengthOfLocation || InitialLocation.Length < minLengthOfLocation)
+            if (bothLocationsBelongToCitiesEnum)
             {
-                throw new RideException("Both locations must be filled");
+                throw new RideException("Locations must match with one city");
             }
         }
 
