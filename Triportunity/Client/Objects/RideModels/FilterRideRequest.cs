32ffaa4205b680d29adcc06b0665e941bd193710
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2024-04-01 21:37:49 -0300

    Corrected namespaces errors

--- Triportunity/Client/Objects/RideModels/FilterRideRequest.cs
+++ Triportunity/Client/Objects/RideModels/FilterRideRequest.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Client.Objects.RideModels
+namespace ClientUI.Objects.RideModels
 {
     public class FilterRideRequest
     {
