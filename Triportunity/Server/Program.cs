3890398a055a0ab31a1f338bf3c50f71fdc657cd
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-04-02 09:24:24 -0300

    Implemented a method to manage the user that has been conected to the server

--- Triportunity/Server/Program.cs
+++ Triportunity/Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 using Server.DataContext;
 
 
@@ -8,6 +10,7 @@ namespace Server
 {
     internal class Program
     {
+        private static bool _listenToNewClients = true;
         public static void Main(string[] args)
         {
             // //For bringing up the db just use this method:
@@ -30,9 +33,37 @@ namespace Server
             //This is how much users could be in a waiting state before entering the system
             receptorSocket.Listen(1000); 
             Console.WriteLine("Waiting for clients...");
+
             
-            
-                
+            Console.ReadKey();
+            int users = 1;
+            while ( _listenToNewClients)
+            {
+
+                Socket transmittorSocket = receptorSocket.Accept();
+                int n = users;
+                new Thread(() => ManageUser(transmittorSocket, users));
+                users++;
+            }
+        }
+
+        private static void ManageUser(Socket transmittorSocket, int actualUser)
+        {
+            Console.WriteLine($@"The user {actualUser} is connected");
+            byte[] buffer = Array.Empty<byte>();
+            int bytesReceived = transmittorSocket.Receive(buffer);
+
+            if (bytesReceived == 0)
+            {
+                Console.WriteLine("The user has been disconnected");
+            }
+            else
+            {
+                string message = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine($@"The user {actualUser} : {message}");
+            }
+            transmittorSocket.Shutdown(SocketShutdown.Both);
+            transmittorSocket.Close();
         }
     }
 }
