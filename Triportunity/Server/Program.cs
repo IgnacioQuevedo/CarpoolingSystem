6b68c556b33395d7a94ab87c099bb92c0c2624f0
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-04-02 08:45:57 -0300

    Declared receptorSocket with its ip,port & maximum pending connections

--- Triportunity/Server/Program.cs
+++ Triportunity/Server/Program.cs
@@ -1,4 +1,7 @@
-﻿using Server.DataContext;
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using Server.DataContext;
 
 
 namespace Server
@@ -7,8 +10,29 @@ namespace Server
     {
         public static void Main(string[] args)
         {
-            //For bringing up the db just use this method:
-            MemoryDatabase database = MemoryDatabase.GetInstance();
+            // //For bringing up the db just use this method:
+            // MemoryDatabase database = MemoryDatabase.GetInstance();
+
+            var localEndPoint = new IPEndPoint(
+                IPAddress.Parse("127.0.0.1"), 4000
+            );
+
+            var receptorSocket = new Socket(
+                // We declare that the socket will use IP
+                AddressFamily.InterNetwork, 
+                // For being possible to use transport protocol we must declare it as a stream socket
+                SocketType.Stream, 
+                ProtocolType.Tcp
+            );
+            
+            //This will connect the socket with the IP and Port define above
+            receptorSocket.Bind(localEndPoint);
+            //This is how much users could be in a waiting state before entering the system
+            receptorSocket.Listen(1000); 
+            Console.WriteLine("Waiting for clients...");
+            
+            
+                
         }
     }
 }
