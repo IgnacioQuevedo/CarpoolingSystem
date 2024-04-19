683185cdfc75bff630a215bf0ac575d296f136c2
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-04-19 01:56:59 -0300

    Protocol is functional and refactored

--- Triportunity/Client/Program.cs
+++ Triportunity/Client/Program.cs
@@ -181,9 +181,7 @@ namespace Client
                 string password = Console.ReadLine();
 
                 string loginInfo = username + ";" + password;
-                _messageInBytes = NetworkHelper.EncodeMsgIntoBytes(loginInfo);
-                
-                NetworkHelper.Send(clientSocket, _messageInBytes);
+                NetworkHelper.SendMessage(clientSocket,loginInfo);
                 //ServiceMethod that will login the user into the app (DO AS A REFACTOR IN A TIME)
                 //_userLogged = UserService.LoginClient(loginRequest);
             }
--- Triportunity/Server/Program.cs
+++ Triportunity/Server/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 using Common;
 
